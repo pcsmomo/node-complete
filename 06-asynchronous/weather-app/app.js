46d76e752dc5868d61c0dda6ec20497e82cf36eb
pcsmomo <dongwoo_333@hanmail.net>
2021-12-20 07:21:14 +1100

    36. Callback Abstraction

--- 06-asynchronous/weather-app/app.js
+++ 06-asynchronous/weather-app/app.js
@@ -1,14 +1,14 @@
 const request = require('request')
 const chalk = require('chalk')
-const { WEATHERSTACK_KEY, MAPBOX_KEY } = require('./config.js')
+const { WEATHERSTACK_KEY } = require('./config.js')
+const geocode = require('./utils/geocode')
 
 // let weatherQuery = 'melbourne' // this can be lat,lng or city
-let geocode = '-37.8142,144.9632' // this can be lat,lng or city
+let location = '-37.8142,144.9632'
 const city = 'melbourne'
 // const city = '12alxkvjacwhat'  // wrong location
 // const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_KEY}&query=${city}`
-const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_KEY}&query=${geocode}`
-const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=${MAPBOX_KEY}`
+const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHERSTACK_KEY}&query=${location}`
 
 // request({ url: url }, (error, response) => {
 //   // console.log(response)
@@ -18,43 +18,24 @@ const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.js
 
 // Weather
 // Using json option
-request({ url: weatherURL, json: true }, (error, response) => {
-  if (error) {
-    console.log(chalk.red('Unable to connect to weather service!'))
-    console.log(`errno: ${error.errno}, code: ${error.code}, hostname: ${error.hostname} `)
-    return
-  } else if (response.body.error) {
-    const resError = response.body.error
-    console.log(chalk.red('Unable to find location'))
-    console.log(`code: ${resError.code}, type: ${resError.type}, type: ${resError.info} `)
-    return
-  }
+// request({ url: weatherURL, json: true }, (error, response) => {
+//   if (error) {
+//     console.log(chalk.red('Unable to connect to weather service!'))
+//     console.log(`errno: ${error.errno}, code: ${error.code}, hostname: ${error.hostname} `)
+//     return
+//   } else if (response.body.error) {
+//     const resError = response.body.error
+//     console.log(chalk.red('Unable to find location'))
+//     console.log(`code: ${resError.code}, type: ${resError.type}, type: ${resError.info} `)
+//     return
+//   }
 
-  // console.log(response.body.current)
-  const { temperature, weather_descriptions } = response.body.current
-  console.log(`${city} is currently ${weather_descriptions[0]} and it is ${temperature} degrees out.`)
-})
+//   // console.log(response.body.current)
+//   const { temperature, weather_descriptions } = response.body.current
+//   console.log(`${city} is currently ${weather_descriptions[0]} and it is ${temperature} degrees out.`)
+// })
 
-// Geocoding
-// Address -> Lat/Lng
-request({ url: geocodeURL, json: true }, (error, response) => {
-  if (error) {
-    console.log(chalk.red('Unable to connect to weather service!'))
-    console.log(`errno: ${error.errno}, code: ${error.code}, hostname: ${error.hostname} `)
-    return
-  } else if (response.body.message) {
-    console.log(chalk.red('Unable to find location'))
-    console.log(`message: ${response.body.message}`)
-    return
-  } else if (response.body.features.length === 0) {
-    console.log(chalk.red('Unable to find location'))
-    return
-  }
-
-  // we will get 5 list from my query 'melbourne'
-  // the first result is the most relative
-  const firstResult = response.body.features[0]
-  const latitude = firstResult.center[1]
-  const longitude = firstResult.center[0]
-  console.log(latitude, longitude)
+geocode('Melbourne', (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
---
+++ 06-asynchronous/weather-app/utils/geocode.js
@@ -0,0 +1,33 @@
+const request = require('request')
+const { MAPBOX_KEY } = require('../config.js')
+
+const geocode = (address, callback) => {
+  const encodedAddress = encodeURIComponent(address)
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodedAddress}.json?access_token=${MAPBOX_KEY}`
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined)
+      return
+    } else if (response.body.message) {
+      callback(response.body.message, undefined)
+      return
+    } else if (response.body.features.length === 0) {
+      callback('Unable to find location', undefined)
+      return
+    }
+
+    const firstResult = response.body.features[0]
+    const latitude = firstResult.center[1]
+    const longitude = firstResult.center[0]
+    const location = firstResult.place_name
+
+    callback(undefined, {
+      latitude,
+      longitude,
+      location
+    })
+  })
+}
+
+module.exports = geocode
