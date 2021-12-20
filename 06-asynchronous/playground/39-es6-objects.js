20bff6a168a3f8e241799cca3b25a64a4b4474a6
pcsmomo <dongwoo_333@hanmail.net>
2021-12-20 16:54:53 +1100

    39. ES6 Aside: Object Property Shorthand and Destructuring

---
+++ 06-asynchronous/playground/39-es6-objects.js
@@ -0,0 +1,31 @@
+// Object property shorthand
+const firstName = 'Andrew'
+const userAge = 27
+
+const user = {
+  firstName,
+  age: userAge,
+  location: 'Philadelphia'
+}
+
+console.log(user)
+
+// Object destructuring
+const product = {
+  label: 'Red notebook',
+  price: 3,
+  stock: 201,
+  salePrice: undefined
+  // rating: 4.2
+}
+
+const { label: productLabel, stock, rating = 5 } = product
+console.log(productLabel)
+console.log(stock)
+console.log(rating)
+
+const transaction = (type, { label, stock }) => {
+  console.log(type, label, stock)
+}
+
+transaction('order', product)
