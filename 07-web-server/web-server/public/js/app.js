c531954583773d7cfbef912f7d239815967aa7ee
pcsmomo <dongwoo_333@hanmail.net>
2021-12-23 07:44:12 +1100

    58. Creating a Search Form

--- 07-web-server/web-server/public/js/app.js
+++ 07-web-server/web-server/public/js/app.js
@@ -1,13 +1,29 @@
 console.log('Client side javascript file is loaded')
 
-fetch('http://localhost:3000/weather?address=melbourne').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error)
-      return
-    }
+const fetchWeather = (address) => {
+  fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+        return
+      }
 
-    console.log(data.location)
-    console.log(data.forecast)
+      console.log(data.location)
+      console.log(data.forecast)
+    })
+  })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const weatherForm = document.querySelector('form')
+  const search = document.querySelector('input')
+
+  weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    console.log(location)
+    fetchWeather(location)
   })
 })
