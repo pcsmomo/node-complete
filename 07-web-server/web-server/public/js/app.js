648bf6f0ba54f676ad264a4677eb4eb120622138
pcsmomo <dongwoo_333@hanmail.net>
2021-12-23 07:32:35 +1100

    57. Browser HTTP Requests with Fetch

--- 07-web-server/web-server/public/js/app.js
+++ 07-web-server/web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side javascript file is loaded')
+
+fetch('http://localhost:3000/weather?address=melbourne').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error)
+      return
+    }
+
+    console.log(data.location)
+    console.log(data.forecast)
+  })
+})
