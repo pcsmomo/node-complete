68b55f09f42bbbb1c545917e98eb0ee7ac3f7542
pcsmomo <dongwoo_333@hanmail.net>
2021-12-23 20:20:27 +1100

    59. Wiring up the User Interface

--- 07-web-server/web-server/public/js/app.js
+++ 07-web-server/web-server/public/js/app.js
@@ -1,29 +1,35 @@
 console.log('Client side javascript file is loaded')
 
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 const fetchWeather = (address) => {
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
   fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         console.log(data.error)
+        messageOne.textContent = data.error
         return
       }
 
       console.log(data.location)
       console.log(data.forecast)
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
     })
   })
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  const weatherForm = document.querySelector('form')
-  const search = document.querySelector('input')
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-  weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+  const location = search.value
 
-    const location = search.value
-
-    console.log(location)
-    fetchWeather(location)
-  })
+  console.log(location)
+  fetchWeather(location)
 })
