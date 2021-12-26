ddc650165c6dcd191fd15301925134bcda67ff33
pcsmomo <dongwoo_333@hanmail.net>
2021-12-26 15:01:05 +1100

    84. Creating a Mongoose Model

--- 11-mongoose-REST-api/task-manager/src/db/mongoose.js
+++ 11-mongoose-REST-api/task-manager/src/db/mongoose.js
@@ -9,7 +9,7 @@ mongoose.connect(`mongodb://${mongoUrl}/${databaseName}`, {
   // useCreateIndex: true
 })
 
-const User = mongoose.model('Users', {
+const User = mongoose.model('User', {
   name: {
     type: String
   },
@@ -26,6 +26,29 @@ const me = new User({
 me.save()
   .then(() => {
     console.log(me)
+  })
+  .catch((error) => {
+    console.log('Error!', error)
+  })
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
+})
+
+const task = new Task({
+  description: 'Learn the Mongoose library',
+  completed: false
+})
+
+task
+  .save()
+  .then(() => {
+    console.log(task)
   })
   .catch((error) => {
     console.log('Error!', error)
