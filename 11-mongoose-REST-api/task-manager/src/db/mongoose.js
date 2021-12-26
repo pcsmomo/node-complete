3c5b118ece8dd63f782da826eb4ebbb2f6f30ee4
pcsmomo <dongwoo_333@hanmail.net>
2021-12-27 08:44:46 +1100

    89. Resource Creation Endpoints: Part I: User Model

--- 11-mongoose-REST-api/task-manager/src/db/mongoose.js
+++ 11-mongoose-REST-api/task-manager/src/db/mongoose.js
@@ -1,67 +1,17 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 const mongoUrl = '127.0.0.1:27017'
 const databaseName = 'task-mananger-api'
 
-mongoose.connect(`mongodb://${mongoUrl}/${databaseName}`, {
-  // https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
-  // useNewUrlParser: true,
-  // useCreateIndex: true
-})
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid')
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"')
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number')
-      }
-    }
-  }
-})
-
-// const me = new User({
-//   name: '  Noah ',
-//   email: 'noah@gmail.com    ',
-//   password: '    11ord1dd    '
-// })
-
-// me.save()
-//   .then(() => {
-//     console.log(me)
-//   })
-//   .catch((error) => {
-//     console.log('Error!', error)
-//   })
+mongoose
+  .connect(`mongodb://${mongoUrl}/${databaseName}`, {
+    // https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
+    // useNewUrlParser: true,
+    // useCreateIndex: true
+  })
+  .then(() => {
+    console.log('database is connected via mongoose...')
+  })
 
 const Task = mongoose.model('Task', {
   description: {
@@ -75,16 +25,17 @@ const Task = mongoose.model('Task', {
   }
 })
 
-const task = new Task({
-  description: '   Vacuum the floor   '
-  // completed: false
-})
+// Create a new task
+// const task = new Task({
+//   description: '   Vacuum the floor   '
+//   // completed: false
+// })
 
-task
-  .save()
-  .then(() => {
-    console.log(task)
-  })
-  .catch((error) => {
-    console.log('Error!', error)
-  })
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task)
+//   })
+//   .catch((error) => {
+//     console.log('Error!', error)
+//   })
--- 11-mongoose-REST-api/task-manager/src/index.js
+++ 11-mongoose-REST-api/task-manager/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+require('./db/mongoose')
+const User = require('./models/user')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,8 +10,16 @@ app.use(express.json())
 app.use(morgan('combined'))
 
 app.post('/users', (req, res) => {
-  console.log(req.body)
-  res.send('testing!')
+  const user = new User(req.body)
+
+  user
+    .save()
+    .then(() => {
+      res.send(user)
+    })
+    .catch((e) => {
+      res.status(400).send(e)
+    })
 })
 
 app.listen(port, () => {
