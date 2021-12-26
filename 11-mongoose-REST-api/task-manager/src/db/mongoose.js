045b5fc3a810098d871dee3cf429acc91706be49
pcsmomo <dongwoo_333@hanmail.net>
2021-12-27 08:51:53 +1100

    90. Resource Creation Endpoints: Part II

--- 11-mongoose-REST-api/task-manager/src/db/mongoose.js
+++ 11-mongoose-REST-api/task-manager/src/db/mongoose.js
@@ -12,30 +12,3 @@ mongoose
   .then(() => {
     console.log('database is connected via mongoose...')
   })
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-})
-
-// Create a new task
-// const task = new Task({
-//   description: '   Vacuum the floor   '
-//   // completed: false
-// })
-
-// task
-//   .save()
-//   .then(() => {
-//     console.log(task)
-//   })
-//   .catch((error) => {
-//     console.log('Error!', error)
-//   })
