496501f149db6f3761d763fbb0878473c7a737ad
pcsmomo <dongwoo_333@hanmail.net>
2022-01-07 08:46:27 +1100

    139. Writing Your Own Tests

--- 16-testing/playground/138-math.js
+++ 16-testing/playground/138-math.js
@@ -5,6 +5,16 @@
 
 const calculateTip = (total, tipPercent = 0.25) => total + total * tipPercent
 
+const fahrenheitToCelsius = (temp) => {
+  return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+  return temp * 1.8 + 32
+}
+
 module.exports = {
-  calculateTip
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit
 }
--- 16-testing/playground/138-math.test.js
+++ 16-testing/playground/138-math.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('@jest/globals')
-const { calculateTip } = require('./138-math.js')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('./138-math.js')
 
 test('Should calculate total with tip!', () => {
   const total = calculateTip(10, 0.3)
@@ -13,6 +13,16 @@ test('Should calculate total with tip!', () => {
 test('Should calculate total with default tip', () => {
   const total = calculateTip(10)
   expect(total).toBe(12.5)
+})
+
+test('Should convert 32 F to 0 C', () => {
+  const celsius = fahrenheitToCelsius(32)
+  expect(celsius).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+  const fahrenheit = celsiusToFahrenheit(0)
+  expect(fahrenheit).toBe(32)
 })
 
 test.skip('This should fail', () => {
