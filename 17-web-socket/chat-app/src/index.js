2041a14b3581e47401f35706c4768b177e9bc073
pcsmomo <dongwoo_333@hanmail.net>
2022-01-09 09:29:22 +1100

    164. Timestamps for Location Messages

--- 17-web-socket/chat-app/src/index.js
+++ 17-web-socket/chat-app/src/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 
-const { generateMessage } = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -34,7 +34,10 @@ io.on('connection', (socket) => {
 
   socket.on('sendLocation', (coords, callback) => {
     // io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
-    io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    io.emit(
+      'locationMessage',
+      generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    )
     callback()
   })
 
